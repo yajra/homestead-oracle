9540bb4246bfb7d4e78b57e30ca5c11d80c02c17
Arjay Angeles <aqangeles@gmail.com>
2015-03-13 14:14:54 +0800

    fix install oci8 script

--- scripts/install-oci8.sh
+++ scripts/install-oci8.sh
@@ -4,6 +4,6 @@ else
 	echo autodetect | pecl install oci8
 	echo 'extension=oci8.so' >> /etc/php5/fpm/php.ini
 	echo 'extension=oci8.so' >> /etc/php5/cli/php.ini
-	echo "env[ORACLE_HOME] = '/u01/app/oracle/product/11.2.0/xe'" > /etc/php5/fpm/php5-fpm.conf
-	echo "env[LD_LIBRARY_PATH] = '/u01/app/oracle/product/11.2.0/xe/lib'" >> /etc/php5/fpm/php5-fpm.conf
+	echo "env[ORACLE_HOME] = '/u01/app/oracle/product/11.2.0/xe'" >> /etc/php5/fpm/php-fpm.conf
+	echo "env[LD_LIBRARY_PATH] = '/u01/app/oracle/product/11.2.0/xe/lib'" >> /etc/php5/fpm/php-fpm.conf
 fi
